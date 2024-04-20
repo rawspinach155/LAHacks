bc02a665701724f9f77027244c039acbe5679d57
stsha <ssha2@uci.edu>
2024-04-20 11:29:38 -0700

    added backend files and reorganized some of the html

---
+++ backend/app.py
@@ -0,0 +1,32 @@
+# Imports
+from flask import Flask, render_template, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import JSON
+import json
+
+# Flask App
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+
+# class MedicalData(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     profile = db.Column(JSON)
+#     # Profile is a json of a dictionary of people
+#     diagnosis = db.Column(JSON)
+
+#     def __repr__(self) -> str:
+#         return f'Patient: {self.id}'@app.route("/",methods=['POST', 'GET'])
+
+app.route("/")
+def index():    
+    return render_template("index.html")
+
+
+
+if __name__ == "__main__":
+    # with app.app_context():
+    #     db.create_all()
+
+    app.run(debug=True, port=5000)
